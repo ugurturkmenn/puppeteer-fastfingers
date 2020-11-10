df8cb82fbf576966603eaa91e589da544da92f57
Ugur Turkmen <ugurturkmen@Ugur-MacBook-Pro.local>
2020-11-10 22:23:12 +0300

    code refactor

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const puppeteer = require('puppeteer');
-
 let wordArrayList;
 
-(async() => {
+( async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: false,
+  });
 
-	const browser = await puppeteer.launch({
-		headless: false,
-		devtools: false,
-	});
+  const page = await browser.newPage();
 
-	const page = await browser.newPage();
+  await page.goto('https://10fastfingers.com/typing-test/turkish', { waitUntil: 'load' });
+  await page.waitForSelector('#row1 span');
 
-	await page.goto('https://10fastfingers.com/typing-test/turkish', { waitUntil: 'load'});
+  const wordList = await page.evaluate(() => {
+    const wordSelector = document.querySelectorAll('#row1 span');
+    const wordArray = Array.from(wordSelector);
 
-	const wordlist = await page.evaluate( () => {
-		const wordSelector = document.querySelectorAll('#row1 span');
-		const wordArray = Array.from(wordSelector);
+    wordArrayList = wordArray.map(word => {
+      return $(word).text();
+    })
+    return wordArrayList;
+  });
 
-		wordArrayList = wordArray.map(word => {
-			return word.innerText;
-		});
+  for(let word of wordList){
+    await page.type('#inputfield', word);
+    await page.keyboard.press('Space');
+  }
 
-		return wordArrayList;
-	});
-
-	for(let word of wordlist){
-		await page.type('#inputfield', word);
-		await page.keyboard.press('Space');
-	}
-
-	await page.waitForSelector('#auswertung-result', { visible: true, timeout: 50000});
-	await page.screenshot({ path: 'score.png'});
-
-})
+  await page.waitForSelector('#auswertung-result', { visible: true, timeout: 50000})
+  await page.screenshot({ path: 'score.png' })
+  
+  // await browser.close()
+})();
